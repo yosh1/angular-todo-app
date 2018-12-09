38fe37b5649c98abbc913d619c8410b4eb4a4539
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-10 08:53:26 +0900

    [Update] First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Task } from './task';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'todo app';
+  task: Task = {
+    id: 1,
+    name: '田中さんにメール'
+  };
 }
